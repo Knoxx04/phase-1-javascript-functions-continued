2131569927860bfceaea56d851863405e218a74e
Knoxx04 <onyiknoxx3@gmail.com>
2022-11-29 08:46:56 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// code your solution here
+
+function saturdayFun(defaultActivity='roller-skate') {
+    return`This Saturday, I want to ${defaultActivity}!`;
+}
+console.log(saturdayFun());
+console.log(saturdayFun("bathe my dog"));
+
+function mondayWork(defActivity='go to the office') {
+    return`This Monday, I will ${defActivity}.`;
+}
+console.log(mondayWork());
+console.log(mondayWork("work from home"));
+
+function wrapAdjective(style=''){
+    return function (condition='special'){
+        return `You are ${style}${condition}${style}!`
+    }
+}
+    
